ae63e159f87edf6904f75cec733940ef2d1cd5b7
sadelcarpio <sadelcarpioa@gmail.com>
2024-06-15 22:45:50 -0500

    parameters

--- matrix-factorization/src/main.py
+++ matrix-factorization/src/main.py
@@ -3,7 +3,7 @@ from src.algorithm import MatrixFactorization
 from src.data import Dataset
 
 if __name__ == '__main__':
-    movielens_subset = Dataset("movielens-20m-dataset/rating.csv", n_most_users=5000, m_most_items=200)
+    movielens_subset = Dataset("movielens-20m-dataset/rating.csv", n_most_users=5000, m_most_items=500)
     train_ds, test_ds = movielens_subset.split_dataset(test_ratio=0.2)
     mf_model = MatrixFactorization(K=100, reg=70.0)
     losses = mf_model.fit(train_ds)
