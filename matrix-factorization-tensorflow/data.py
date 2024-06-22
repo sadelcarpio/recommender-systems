4727562f83801d477a6ffa68fa4aa32852f4bec8
sadelcarpio <sadelcarpioa@gmail.com>
2024-06-22 00:07:23 -0500

    Actual data used

---
+++ matrix-factorization-tensorflow/data.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+
+class Dataset:
+    """
+    Common dataset structure for collaborative filtering recommendations on MovieLens dataset.
+    Structure: userId, movieId, rating
+    """
+    def __init__(self, dataset_csv_path: str, n_most_users: int, m_most_items: int):
+        self.dataset = pd.read_csv(dataset_csv_path)
+        most_common_users = self.dataset.userId.value_counts().head(n_most_users)
+        most_common_movies = self.dataset.movieId.value_counts().head(m_most_items)
+        self.dataset = self.dataset[(self.dataset.userId.isin(most_common_users.index)) &
+                                    (self.dataset.movieId.isin(most_common_movies.index))]
+        self.dataset['userIdOrdered'] = self.dataset['userId'].astype('category').cat.codes
+        self.dataset['movieIdOrdered'] = self.dataset['movieId'].astype('category').cat.codes
+
+    def split_dataset(self, test_ratio: float) -> tuple:
+        """
+        Splits dataset randomly into train, test.
+        :param test_ratio: Ratio of test samples, between 0 - 1
+        :return: tuple of train_dataset, test_dataset
+        """
+        dataset_train = self.dataset.sample(frac=(1 - test_ratio), random_state=42)
+        dataset_test = self.dataset.drop(dataset_train.index)
+        return dataset_train, dataset_test
--- matrix-factorization-tensorflow/mf.py
+++ matrix-factorization-tensorflow/mf.py
@@ -1,25 +1,28 @@
+from data import Dataset
+
 import numpy as np
 from keras import layers, Model, regularizers, optimizers
 
-N = 100
-M = 10
-K = 5
+dataset = Dataset("movielens-20m-dataset/rating.csv", n_most_users=1000, m_most_items=100)
+train, test = dataset.split_dataset(0.2)
+mu_train = train["rating"].mean()
+mu_test = test["rating"].mean()
 
-ratings_range = np.arange(0.0, 5.5, 0.5)
-
-train_w = np.random.choice(N, size=1000, replace=True)
-train_u = np.random.choice(M, size=1000, replace=True)
-train_r = np.random.choice(ratings_range, size=1000, replace=True)
+N = train.userId.nunique()
+M = train.movieId.nunique()
+K = 20
 
 w = layers.Input(shape=(1,))
 u = layers.Input(shape=(1,))
-w_embedding = layers.Embedding(input_dim=N, output_dim=K, embeddings_regularizer=regularizers.l2(20.0))(w)
-b_embedding = layers.Embedding(input_dim=N, output_dim=1, embeddings_regularizer=regularizers.l2(20.0))(w)
-u_embedding = layers.Embedding(input_dim=M, output_dim=K, embeddings_regularizer=regularizers.l2(20.0))(u)
-c_embedding = layers.Embedding(input_dim=M, output_dim=1, embeddings_regularizer=regularizers.l2(20.0))(u)
+b_embedding = layers.Embedding(input_dim=N, output_dim=1, embeddings_regularizer=regularizers.l2(3e-4))(w)
+w_embedding = layers.Embedding(input_dim=N, output_dim=K, embeddings_regularizer=regularizers.l2(3e-4))(w)
+u_embedding = layers.Embedding(input_dim=M, output_dim=K, embeddings_regularizer=regularizers.l2(3e-4))(u)
+c_embedding = layers.Embedding(input_dim=M, output_dim=1, embeddings_regularizer=regularizers.l2(3e-4))(u)
 mf_layer = layers.Dot(axes=2)([w_embedding, u_embedding])
 bias_layer = layers.Add()([mf_layer, b_embedding, c_embedding])
 model = Model(inputs=[w, u], outputs=bias_layer)
 
-model.compile(loss='mse', optimizer=optimizers.SGD(learning_rate=0.001))
-model.fit([train_w, train_u], train_r, epochs=100)
+model.compile(loss='mse', optimizer=optimizers.Adam(learning_rate=0.0001))
+model.fit([train["userIdOrdered"], train["movieIdOrdered"]], train["rating"] - mu_train,
+          validation_data=([test["userIdOrdered"], test["movieIdOrdered"]], test["rating"] - mu_train),
+          epochs=50)
