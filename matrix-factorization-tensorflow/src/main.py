632d99b4cfa77628e56a2e613b9ef8e063ed3ba1
sadelcarpio <sadelcarpioa@gmail.com>
2024-07-02 23:02:44 -0500

    Using Deep Learning with embeddings

--- matrix-factorization-tensorflow/src/main.py
+++ matrix-factorization-tensorflow/src/main.py
@@ -1,5 +1,7 @@
+from keras import optimizers
+
 from src.data import Dataset
-from src.model.mf import create_model
+from src.model.mf import MFModel
 
 dataset = Dataset("movielens-20m-dataset/rating.csv", n_most_users=2000, m_most_items=200)
 train, test = dataset.split_dataset(0.2)
@@ -9,9 +11,11 @@ mu_test = test["rating"].mean()
 N = train.userIdOrdered.nunique()
 M = train.movieIdOrdered.nunique()
 
-model = create_model(k=60, m=M, n=N, reg=3e-4)
+model = MFModel(k=60, m=M, n=N, reg=3e-4, deep=True)
 
+
+model.compile(loss='mse', optimizer=optimizers.Adam(learning_rate=0.0001))
 history = model.fit([train["userIdOrdered"], train["movieIdOrdered"]], train["rating"] - mu_train,
                     validation_data=([test["userIdOrdered"], test["movieIdOrdered"]], test["rating"] - mu_train),
-                    batch_size=128,
+                    batch_size=256,
                     epochs=50)
--- matrix-factorization-tensorflow/src/model/mf.py
+++ matrix-factorization-tensorflow/src/model/mf.py
@@ -1,33 +1,38 @@
 from keras import layers, Model, regularizers, optimizers
 
 
-def create_model(k: int, m: int, n: int, reg: float = 0.0):
-    """
-    Create a MF model with bias and regularization
-    :param k: Dimension of embeddings a.k.a number of features per user / item
-    :param m: Number of unique items
-    :param n: Number of unique users
-    :param reg: regularization parameter
-    :return: Keras model.
-    """
-    w = layers.Input(shape=(1,))
-    u = layers.Input(shape=(1,))
-    # w produces a K-length vector embedding per user
-    w_embedding = layers.Embedding(input_dim=n, output_dim=k, embeddings_regularizer=regularizers.l2(reg))(w)
-    # b_i is an 1-1 embedding of user inputs, accounting for bias on each user
-    b_embedding = layers.Embedding(input_dim=n, output_dim=1, embeddings_regularizer=regularizers.l2(reg))(w)
-    # u produces a K-length vector embedding per item
-    u_embedding = layers.Embedding(input_dim=m, output_dim=k, embeddings_regularizer=regularizers.l2(reg))(u)
-    # c_j is a 1-1 embedding of item inputs, accounting for bias on each item
-    c_embedding = layers.Embedding(input_dim=m, output_dim=1, embeddings_regularizer=regularizers.l2(reg))(u)
-    # Dot computes dot product along axis 2.
-    # Given: w_embedding.shape = [None, 1, K]. w_embedding = [[w1, w2, w3], ...], w1 = [w11, w12, w13]
-    # u_embedding.shape = [None, 1, K] = [[u1, u2, u3], ...], u1 = [u11, u12, u13]
-    # then mf_layer.shape = [None, 1, 1], mf_layer = [[[dot(w1.T, u1)], [dot(w2.T, u2)], [dot(w3.T, u3)]], ...]
-    mf_layer = layers.Dot(axes=2)([w_embedding, u_embedding])
-    # Add the mf result with b and c bias
-    # dot(w.T, u) + b_i + c_j
-    bias_layer = layers.Add()([mf_layer, b_embedding, c_embedding])
-    model = Model(inputs=[w, u], outputs=bias_layer)
-    model.compile(loss='mse', optimizer=optimizers.Adam(learning_rate=0.0001))
-    return model
+class MFModel(Model):
+    def __init__(self, n: int, m: int, k: int, reg: float = 0.0, deep: bool = False):
+        super().__init__()
+        self.w = layers.Input(shape=(1,))
+        self.u = layers.Input(shape=(1,))
+        # w produces a K-length vector embedding per user
+        self.w_embedding = layers.Embedding(input_dim=n, output_dim=k,
+                                            embeddings_regularizer=regularizers.l2(reg))(self.w)
+        # u produces a K-length vector embedding per item
+        self.u_embedding = layers.Embedding(input_dim=m, output_dim=k,
+                                            embeddings_regularizer=regularizers.l2(reg))(self.u)
+        if deep:
+            self.concat_features = layers.Concatenate()([self.w_embedding, self.u_embedding])
+            self.dense_1 = layers.Dense(units=k // 2, activation='relu')(self.concat_features)
+            self.dense_2 = layers.Dense(units=1, activation='linear')(self.dense_1)
+            self.model = Model(inputs=[self.w, self.u], outputs=self.dense_2)
+        else:
+            # b_i is an 1-1 embedding of user inputs, accounting for bias on each user
+            self.b_embedding = layers.Embedding(input_dim=n, output_dim=1,
+                                                embeddings_regularizer=regularizers.l2(reg))(self.w)
+            # c_j is a 1-1 embedding of item inputs, accounting for bias on each item
+            self.c_embedding = layers.Embedding(input_dim=m, output_dim=1,
+                                                embeddings_regularizer=regularizers.l2(reg))(self.u)
+            # Dot computes dot product along axis 2.
+            # Given: w_embedding.shape = [None, 1, K]. w_embedding = [[w1, w2, w3], ...], w1 = [w11, w12, w13]
+            # u_embedding.shape = [None, 1, K] = [[u1, u2, u3], ...], u1 = [u11, u12, u13]
+            # then mf_layer.shape = [None, 1, 1], mf_layer = [[[dot(w1.T, u1)], [dot(w2.T, u2)], [dot(w3.T, u3)]], ...]
+            self.mf_layer = layers.Dot(axes=2)([self.w_embedding, self.u_embedding])
+            # Add the mf result with b and c bias
+            # dot(w.T, u) + b_i + c_j
+            self.bias_layer = layers.Add()([self.mf_layer, self.b_embedding, self.c_embedding])
+            self.model = Model(inputs=[self.w, self.u], outputs=self.bias_layer)
+
+    def call(self, inputs):
+        return self.model(inputs)
