7f0baf3f102b1dfbd7d67672d7d6b0d3f0080909
sadelcarpio <sadelcarpioa@gmail.com>
2024-10-20 15:26:48 -0500

    Good results with 0.6430 train, 0.6536 val

--- matrix-factorization-tensorflow/src/main_autorec.py
+++ matrix-factorization-tensorflow/src/main_autorec.py
@@ -8,10 +8,10 @@ n_most_users = 20000
 n_most_items = 2000
 
 dataset = Dataset("movielens-20m-dataset/rating.csv", n_most_users=n_most_users, m_most_items=n_most_items)
-train_ds, test_ds = dataset.sparse_dataset(test_ratio=.2, batch_size=128)
+train_ds, test_ds = dataset.sparse_dataset(test_ratio=.2, batch_size=64)
 print(f"Number of users: {dataset.n_users}")
 print(f"Number of items: {dataset.n_items}")
-model = AutoRecommender(m=dataset.n_items, k=100)
+model = AutoRecommender(m=dataset.n_items, k=150)
 model.compile(loss=custom_sparse_mse, optimizer=optimizers.Adam(learning_rate=0.0001))
 print(model.summary())
 model.fit(train_ds, validation_data=test_ds, epochs=100)
