567ceff6344699693d8beed782e326c96f801cca
sadelcarpio <sadelcarpioa@gmail.com>
2024-10-29 21:51:31 -0500

    Computing scores via argmax

--- rbm-tensorflow/model.py
+++ rbm-tensorflow/model.py
@@ -100,10 +100,10 @@ class CategoricalRBM(Model):
     def sample_v(self, h):
         linear = tf.tensordot(h, self.w, axes=[[1], [2]]) + self.b
         p_v = tf.math.softmax(linear)
-        p_v_reshaped = tf.reshape(p_v, [-1, self.num_classes])
+        p_v_reshaped = tf.stop_gradient(tf.reshape(p_v, [-1, self.num_classes]))
         sample_indices = tf.stop_gradient(tf.random.categorical(tf.math.log(p_v_reshaped), num_samples=1))
-        sample_indices_reshaped = tf.reshape(sample_indices, [-1, self.visible_units])
-        v = tf.one_hot(sample_indices_reshaped, depth=self.num_classes)
+        sample_indices_reshaped = tf.stop_gradient(tf.reshape(sample_indices, [-1, self.visible_units]))
+        v = tf.stop_gradient(tf.one_hot(sample_indices_reshaped, depth=self.num_classes))
         return v
 
     def call(self, inputs, training=False, mask=None):
@@ -133,11 +133,20 @@ class CategoricalRBM(Model):
             mask = tf.reduce_sum(dense_v, axis=-1, keepdims=True) == 0
             v_prime = tf.where(mask, tf.zeros_like(v_prime), v_prime)
             loss = tf.reduce_mean(self.free_energy(dense_v) - self.free_energy(v_prime))
-        diff = tf.reduce_mean(tf.math.abs(dense_v - v_prime))
+        diff = tf.reduce_mean(tf.abs(dense_v - v_prime))
+        # Get labels
+        v_labels = tf.argmax(dense_v, axis=2)
+        v_prime_labels = tf.argmax(v_prime, axis=2)
+        # Get indices of nonzero
+        indices = tf.where(tf.reduce_sum(dense_v, axis=-1) != 0)
+        # Get nonzero ratings
+        v_ratings = 0.5 * tf.cast(tf.gather_nd(v_labels, indices), dtype=tf.float32) + 0.5
+        v_prime_ratings = 0.5 * tf.cast(tf.gather_nd(v_prime_labels, indices), dtype=tf.float32) + 0.5
+        mae = tf.reduce_mean(tf.abs(v_ratings - v_prime_ratings))
         trainable_vars = self.trainable_variables
         gradients = tape.gradient(loss, trainable_vars)
         self.optimizer.apply_gradients(zip(gradients, trainable_vars))
-        return {'loss': loss, 'v-v_prime': diff}
+        return {'loss': loss, 'v-v_prime': diff, 'mae-ratings': mae}
 
     def test_step(self, data):
         v, v = data
@@ -145,6 +154,15 @@ class CategoricalRBM(Model):
         v_prime = self(dense_v, training=False)
         mask = tf.reduce_sum(dense_v, axis=-1, keepdims=True) == 0
         v_prime = tf.where(mask, tf.zeros_like(v_prime), v_prime)
-        diff = tf.reduce_mean(tf.math.abs(dense_v - v_prime))
+        # Get labels
+        v_labels = tf.argmax(dense_v, axis=2)
+        v_prime_labels = tf.argmax(v_prime, axis=2)
+        # Get indices of nonzero
+        indices = tf.where(tf.reduce_sum(dense_v, axis=-1) != 0)
+        # Get nonzero ratings
+        v_ratings = 0.5 * tf.cast(tf.gather_nd(v_labels, indices), dtype=tf.float32) + 0.5
+        v_prime_ratings = 0.5 * tf.cast(tf.gather_nd(v_prime_labels, indices), dtype=tf.float32) + 0.5
+        mae = tf.reduce_mean(tf.abs(v_ratings - v_prime_ratings))
+        diff = tf.reduce_mean(tf.abs(dense_v - v_prime))
         loss = tf.reduce_mean(self.free_energy(dense_v) - self.free_energy(v_prime))
-        return {'loss': loss, 'v-v_prime': diff}
+        return {'loss': loss, 'v-v_prime': diff, 'mae-ratings': mae}
